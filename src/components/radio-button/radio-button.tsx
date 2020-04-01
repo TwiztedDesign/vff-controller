ad8ae539e9bcbd53527ed94c3e8f4ba2bc45dd36
Alex_Bartfeld <Welcome1>
2020-04-02 00:15:03 +0300

    refactor radio button

--- src/components/radio-button/radio-button.tsx
+++ src/components/radio-button/radio-button.tsx
@@ -15,12 +15,12 @@ export class RadioButton {
   @Prop({reflect: true, mutable: true}) checked = false;
 
   @Watch('checked')
-  validateChecked(newValue: boolean) {
+  validateCheckedPropChange(newValue: boolean) {
     this.radioButton.checked = newValue;
-    if (newValue) {
+    if (newValue) { // If one button is enabled, others should disable themselves. Let's notify them.
       this.radioButtonStateChange.emit({
         origin: this.radioButton,
-        name: this.name,
+        name: (this.name || this.el.getAttribute('name')), // When changed from DOM "name" prop will be null.
         checked: this.checked
       });
     }
@@ -34,7 +34,7 @@ export class RadioButton {
   }) radioButtonStateChange: EventEmitter;
 
   @Listen('radioButtonStateChange', {target: 'document'})
-  handleRadioButtonClicked(event: CustomEvent) {
+  handleRadioButtonStateChange(event: CustomEvent) {
     const {origin, name} = event.detail;
     if (origin === this.radioButton) return;
     if (name === this.name) {
@@ -48,11 +48,11 @@ export class RadioButton {
 
   componentDidLoad() {
     this.radioButton = this.el.shadowRoot.querySelector('input');
-    this.radioButton.checked = this.checked;
+    this.radioButton.checked = this.checked; // In case attribute was set in HTML on initial render.
   }
 
   handleClick() {
-    !this.checked && (this.checked = true); // radio button can't disable it'self
+    !this.checked && (this.checked = true); // Radio button can't disable itself.
   }
 
   render() {
