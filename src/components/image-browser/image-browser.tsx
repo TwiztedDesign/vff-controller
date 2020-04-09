45e6d7d632663ebd9dffad90f91391b45b69ec7a
Alex_Bartfeld <Welcome1>
2020-04-09 14:08:26 +0300

    remove search bar functionality

--- src/components/image-browser/image-browser.tsx
+++ src/components/image-browser/image-browser.tsx
@@ -1,5 +1,5 @@
 import {Component, Host, h, Element, Prop, Watch, State} from '@stencil/core';
-import {getImage, readFileAsync} from "../../utils/utils";
+import {readFileAsync} from "../../utils/utils";
 
 @Component({
   tag: 'vff-image-browser',
@@ -9,10 +9,7 @@ import {getImage, readFileAsync} from "../../utils/utils";
 export class ImageBrowser {
   private previewZone: HTMLElement;
 
-  @State() searchBarInputValue = '';
   @State() previewList = [];
-  @State() isFetchError: boolean = false;
-  @State() isFetchingFile: boolean = false;
 
   @Prop({mutable: true}) selectedFiles: File[] = [];
 
@@ -35,15 +32,6 @@ export class ImageBrowser {
       });
     } else if (newValue.length === 0) {
       this.previewList = [];
-    }
-  }
-
-  @Watch('isFetchError')
-  handleErrorStateChange(newValue: boolean) {
-    if (newValue) {
-      setTimeout(() => {
-        this.isFetchError = false;
-      }, 5000)
     }
   }
 
@@ -84,52 +72,10 @@ export class ImageBrowser {
     this.selectedFiles = this.selectedFiles.filter(lf => lf !== file);
   }
 
-  renderSearchBar() {
-    const disabled = this.isFetchingFile;
-
-    return (
-      <div id="search-bar">
-        <input placeholder="place url to grab an image ..." id="search-bar__input" type="url"
-               disabled={disabled}
-               value={this.searchBarInputValue}
-               onChange={(e) => {
-                 this.searchBarInputValue = (e.target as HTMLInputElement).value;
-               }}
-               onKeyUp={(e) => {
-                 if (e.code === 'Enter') {
-                   (this.el.shadowRoot.querySelector('#search-bar__btn') as HTMLElement).click();
-                 }
-               }}/>
-        <button id="search-bar__btn" type="button"
-                disabled={disabled}
-                onClick={() => {
-                  const value = encodeURI(this.searchBarInputValue.trim());
-                  if (!value) return;
-                  this.isFetchingFile = true;
-                  getImage(value)
-                    .then((file) => {
-                      this.searchBarInputValue = '';
-                      this.addFiles([new File([file], `image-${Date.now()}`, {})]);
-                    })
-                    .catch(() => {
-                      this.isFetchError = true;
-                    })
-                    .finally(() => {
-                      this.isFetchingFile = false;
-                    });
-                }}>
-          Select Image
-        </button>
-      </div>
-    );
-  }
-
-  renderDropZone() {
+  render() {
     let content = null;
 
-    if (this.isFetchingFile) {
-      content = <div id="loader">Loading</div>;
-    } else if (this.selectedFiles.length === 0) {
+    if (this.selectedFiles.length === 0) {
       content = <label htmlFor="preview__input" id="preview__instructions">
         Drop images here or <span id="click">click</span> to select.
       </label>;
@@ -148,29 +94,16 @@ export class ImageBrowser {
     }
 
     return (
-      <div id="preview">
-        <input id="preview__input" type="file" multiple accept="image/*"
-               onChange={(e) => {
-                 const target = e.target as HTMLInputElement;
-                 this.addFiles(target.files)
-               }}/>
-        {content}
-      </div>
-    )
-  }
-
-  render() {
-    return (
       <Host>
-        {this.renderSearchBar()}
-        {this.renderDropZone()}
-        {
-          this.isFetchError ?
-            <div id="error-msg" onClick={() => this.isFetchError = false}>
-              There was an error getting your image, check the console to see more information.
-            </div> : null
-        }
+        <div id="preview">
+          <input id="preview__input" type="file" multiple accept="image/*"
+                 onChange={(e) => {
+                   const target = e.target as HTMLInputElement;
+                   this.addFiles(target.files)
+                 }}/>
+          {content}
+        </div>
       </Host>
-    );
+    )
   }
 }
